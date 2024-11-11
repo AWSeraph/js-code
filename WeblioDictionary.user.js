70c9319b7e5c2a81eb0d663d488ca45bd0f831f3
AWSeraph <49120792+AWSeraph@users.noreply.github.com>
2024-11-12 04:43:12 +0900

    Create WeblioDictionary.user.js

---
+++ WeblioDictionary.user.js
@@ -0,0 +1,35 @@
+// ==UserScript==
+// @name     Weblio Dictionary
+// @version  1
+// @include  /ncode\.syosetu\.com\/n\d[^\/]+\/\d+/
+// @grant    none
+// ==/UserScript==
+
+let api = 'https://api.weblio.jp/act/quote/v_1_0?q=';
+let mouX = 0;
+let mouY = 0;
+let inDiv = false;
+
+document.onmousemove = (e) =>{
+  mouX = e.layerX;
+  mouY = e.layerY;
+}
+
+window.onkeyup = (e) =>{
+  console.log(e);
+  if(e.key != 'Control') return;
+  
+  let selText = window.getSelection().toString();
+  if(!selText || selText.length <= 0) return;
+  
+  let DIV = document.createElement('div');
+  
+  DIV.innerHTML = `<div style="width: 315px; height: 222px; position: absolute; z-index: 2147483647; left: ${mouX}px; top: ${mouY}px;">
+<iframe src="${api}${selText}" width="320" height="240" border="0" style="background-color: white;"></iframe>
+</div>`
+  DIV.onmouseenter = () => inDiv = true;
+  DIV.onmouseout = () => inDiv = false;
+  document.onmouseup = () => {if(!inDiv) DIV.remove();}
+  
+  document.body.appendChild(DIV);
+}
